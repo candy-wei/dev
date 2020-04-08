106dcafd742b743650bf46b1486daa27a23e5a8f
daihongwei <candy009@foxmail.com>
2020-04-08 15:07:05 +0800

    Fix

--- mobile-api/src/main/java/com/ningyuan/mobile/dto/TaskDto.java
+++ mobile-api/src/main/java/com/ningyuan/mobile/dto/TaskDto.java
@@ -6,7 +6,11 @@ public class TaskDto {
     // 任务内容描述
     private String content;
     // 已完成，未完成，已开启
-    private String completeStatus;
+    private String baseTask;
+
+    private String specialTask;
+
+    private String dailyTask;
 
     public String getTitle() { return title; }
 
@@ -16,7 +20,15 @@ public class TaskDto {
 
     public void setContent(String content) { this.content = content; }
 
-    public String getCompleteStatus() { return completeStatus; }
+    public String getBaseTask() { return baseTask; }
 
-    public void setCompleteStatus(String completeStatus) { this.completeStatus = completeStatus; }
+    public void setBaseTask(String baseTask) { this.baseTask = baseTask; }
+
+    public String getSpecialTask() { return specialTask; }
+
+    public void setSpecialTask(String specialTask) { this.specialTask = specialTask; }
+
+    public String getDailyTask() { return dailyTask; }
+
+    public void setDailyTask(String dailyTask) { this.dailyTask = dailyTask; }
 }
